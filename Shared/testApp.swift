559d6f44a73bd8ba2db47e5c7103025a3a16ef47
Darinka <dpolevyk@post.cz>
2021-10-23 10:00:18 +0200

    test

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Darinka on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
